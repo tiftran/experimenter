0e51f0f93ff88ce35a97a905a58e6cdac285394e
tiftran <ttran@mozilla.com>
2020-02-27 10:54:05 -0800

    temp

--- app/experimenter/base/management/commands/generate-docs.py
+++ app/experimenter/base/management/commands/generate-docs.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.core.management import call_command
 from django.template.loader import render_to_string
 from django.core.management.base import BaseCommand
+from rest_framework.schemas.openapi import SchemaGenerator
 
 
 logger = logging.getLogger()
@@ -29,8 +30,25 @@ class Command(BaseCommand):
         return output.read()
 
     @staticmethod
+    def generate_schema():
+        generator = SchemaGenerator(title='Experimenter')
+        schema = generator.get_schema()
+        paths = schema["paths"]
+        for path in paths:
+            if  "design" in path:
+                paths[path]["tags"] = "design"
+            else:
+                paths[path]["tags"] = "experiments"
+        
+        return json.dumps(schema, indent=2)
+
+    @staticmethod
     def generate_docs(options):
-        api_json = Command.read_json_doc()
+
+
+
+        #api_json = Command.read_json_doc()
+        api_json = Command.generate_schema()
         docs_dir = os.path.join(settings.BASE_DIR, "docs")
         schema_json_path = os.path.join(docs_dir, "openapi-schema.json")
         swagger_html_path = os.path.join(docs_dir, "swagger-ui.html")
